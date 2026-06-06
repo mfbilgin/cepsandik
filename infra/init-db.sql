7349d15dd9b6aea23c7a17dd54f9fa3c400196c9
Muhammet Bilgin <mf.bilgin0@hotmail.com>
2026-06-07 01:22:10 +0300

    chore(infra): bulletin-board service Postgres'ten MongoDB'ye taşındı
    - application.properties: Postgres bağlantısı kaldırıldı, MongoDB URI'sı kullanılıyor.
    - init-db.sql: bulletindb veritabanı ve yetkilendirmesi kaldırıldı.
    - docker-compose.prod.yaml + docker-compose.yaml: bulletin-board-service mongo'ya bağlandı, mongo servisi eklendi.

--- infra/init-db.sql
+++ infra/init-db.sql
@@ -4,7 +4,6 @@
 CREATE DATABASE userdb;
 CREATE DATABASE communitydb;
 CREATE DATABASE electiondb;
-CREATE DATABASE bulletindb;
 CREATE DATABASE notificationdb;
 
 -- Create application user and grant privileges
@@ -13,7 +12,6 @@ CREATE USER cepsandik_user WITH PASSWORD 'REDACTED_DB_PASSWORD';
 GRANT ALL PRIVILEGES ON DATABASE userdb TO cepsandik_user;
 GRANT ALL PRIVILEGES ON DATABASE communitydb TO cepsandik_user;
 GRANT ALL PRIVILEGES ON DATABASE electiondb TO cepsandik_user;
-GRANT ALL PRIVILEGES ON DATABASE bulletindb TO cepsandik_user;
 GRANT ALL PRIVILEGES ON DATABASE notificationdb TO cepsandik_user;
 
 -- Grant schema privileges (required for PostgreSQL 15+)
@@ -24,9 +22,6 @@ GRANT ALL ON SCHEMA public TO cepsandik_user;
 GRANT ALL ON SCHEMA public TO cepsandik_user;
 
 \c electiondb
-GRANT ALL ON SCHEMA public TO cepsandik_user;
-
-\c bulletindb
 GRANT ALL ON SCHEMA public TO cepsandik_user;
 
 \c notificationdb
